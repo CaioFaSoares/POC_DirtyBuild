53c73e73b501431dd7069ce7bdab379a4bd18a70
Caio Soares <caiofasoares@gmail.com>
2023-06-08 19:41:07 -0300

    Initial Commit

---
+++ POC_DirtyBuild/POC_DirtyBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_DirtyBuildApp.swift
+//  POC_DirtyBuild
+//
+//  Created by Caio Soares on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_DirtyBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
